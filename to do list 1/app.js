0b952a8c50943ca22d7a90bb696e50e82ab4c6b8
KArtikeyShukla2511 <Kartikey.shukla@zeuslearning.com>
2023-09-07 14:17:03 +0530

    to do list 1 updates

--- to do list 1/app.js
+++ to do list 1/app.js
@@ -1,15 +1,41 @@
 const ul = document.querySelector("ul");
-const liItems = document.querySelectorAll("li");
-const btnAdd = document.querySelector("button");
-btnAdd.addEventListener("click", () => {
-  const li = document.createElement("li");
-  li.textContent = "something new to do";
-  ul.prepend(li);
-});
+      const liItems = document.querySelectorAll("li");
+      const btnAdd = document.querySelector("button");
+      const newTodoInput = document.getElementById("newTodo");
 
-ul.addEventListener("click", (e) => {
-  console.log(e.target);
-  if (e.target.tagName === "LI") {
-    e.target.style.textDecoration = "line-through";
-  }
-});
+      btnAdd.addEventListener("click", () => {
+        const taskText = newTodoInput.value.trim();
+        if (taskText !== "") {
+          const li = document.createElement("li");
+          li.textContent = taskText;
+          li.addEventListener("click", toggleTaskCompletion);
+          ul.appendChild(li);
+          newTodoInput.value = "";
+          btnAdd.disabled = true;
+        }
+      });
+
+      ul.addEventListener("click", (e) => {
+        if (e.target.tagName === "LI") {
+          toggleTaskCompletion(e.target);
+        }
+      });
+
+      // Function to toggle task completion (strikethrough)
+      function toggleTaskCompletion(taskItem) {
+        taskItem.style.textDecoration = taskItem.style.textDecoration
+          ? ""
+          : "line-through";
+      }
+
+      // Function to handle input change and button state
+      function handleInputChange() {
+        const taskText = newTodoInput.value.trim();
+        btnAdd.disabled = taskText === "";
+      }
+
+      // Add event listener for input changes
+      newTodoInput.addEventListener("input", handleInputChange);
+
+      // Initial button state setup
+      btnAdd.disabled = true;
